31c429a35cdf06ba7214369f0a93ec1a19cf7345
CarlosVO <carlosvogi@gmail.com>
2024-01-17 18:16:50 +0100

    Deat: search in proggress

--- src/app/pages/search-results/search-results.component.ts
+++ src/app/pages/search-results/search-results.component.ts
@@ -1,16 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Show } from '../../shared/models/shows';
 import { IonContent, IonInfiniteScroll } from '@ionic/angular/standalone';
 import { ShowCardComponent } from '../../shared/components/show-card/show-card.component';
 import { SharedDataService } from '../../core/services/shared-data/shared-data.service';
 import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.scss'],
   standalone: true,
-  imports: [IonInfiniteScroll, ShowCardComponent, IonContent],
+  imports: [CommonModule, IonInfiniteScroll, ShowCardComponent, IonContent],
 })
 export class SearchResultsComponent implements OnInit {
   shows: Show[] | [] = [];
